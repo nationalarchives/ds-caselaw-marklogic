97a35c2145d1e6c30bd1a1a8cd4be32fcaa36f9e
James Harries <dxw@pancakeuprising.com>
2024-06-26 18:35:34 +0100

    Chore: Update populate scripts to add collection and update readme to reference them

--- development_scripts/populate_from_caselaw.py
+++ development_scripts/populate_from_caselaw.py
@@ -32,7 +32,7 @@ for url in urls:
     print("got xml")
 
     response = requests.put(
-        f"http://admin:admin@localhost:8011/LATEST/documents?uri={ml_url}",
+        f"http://admin:admin@localhost:8011/LATEST/documents?uri={ml_url}&collection=judgment",
         data=xml,
     )
     response.raise_for_status()
