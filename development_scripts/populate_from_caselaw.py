533d6655689dcf2100e01eb37f0c33ce76c8f86b
David McKee <david.mckee@dxw.com>
2025-02-26 16:07:10 +0000

    Add properties metadata for one file; we can change how the others behave later

--- development_scripts/populate_from_caselaw.py
+++ development_scripts/populate_from_caselaw.py
@@ -12,6 +12,8 @@ Usage:
 - Run the script.
 """
 
+from pathlib import Path
+
 import requests
 
 urls = [
@@ -19,7 +21,6 @@ urls = [
     "ewca/civ/2003/1048",
     "ewca/civ/2003/1489",
     "ewca/civ/2003/48",
-    "eat/2023/1",
     "eat/2023/2",
 ]
 
@@ -37,3 +38,22 @@ for url in urls:
     )
     response.raise_for_status()
     print("added to local Marklogic db")
+
+for filename in ["eat-2023-1"]:
+    ml_url = "/" + filename.replace("-", "/") + ".xml"
+    with Path(f"development_scripts/fixtures/{filename}-content.xml").open() as f:
+        content = f.read()
+    with Path(f"development_scripts/fixtures/{filename}-properties.xml").open() as f:
+        properties = f.read()
+    response = requests.put(
+        f"http://admin:admin@localhost:8011/LATEST/documents?uri={ml_url}&collection=judgment",
+        data=content,
+    )
+    response.raise_for_status()
+    response = requests.put(
+        f"http://admin:admin@localhost:8011/LATEST/documents?uri={ml_url}&category=properties",
+        data=properties,
+    )
+    print(response.content)
+    response.raise_for_status()
+    print(f"added {filename} to local Marklogic db")
